e95c19ca2b1e8bbc9eb9b6edf4b7ab985bac7493
Chris Bates <christopher.andrew.bates@gmail.com>
2015-04-16 22:56:42 -0700

    changed dirs

--- scripts/ec2/run_all.sh
+++ scripts/ec2/run_all.sh
@@ -1,15 +1,15 @@
 # Setup Python
-chmod +x /root/metricboard-server/scripts/ec2/setup_python.sh
-/root/metricboard-server/scripts/ec2/setup_python.sh
+chmod +x /root/feelsjournal/scripts/ec2/setup_python.sh
+/root/feelsjournal/scripts/ec2/setup_python.sh
 
 # Setup Environment Settings
-chmod +x /root/metricboard-server/scripts/ec2/setup_env.sh
-/root/metricboard-server/scripts/ec2/setup_env.sh
+chmod +x /root/feelsjournal/scripts/ec2/setup_env.sh
+/root/feelsjournal/scripts/ec2/setup_env.sh
 
 # Setup Gunicorn
-chmod +x /root/metricboard-server/scripts/ec2/setup_gunicorn.sh
-/root/metricboard-server/scripts/ec2/setup_gunicorn.sh
+chmod +x /root/feelsjournal/scripts/ec2/setup_gunicorn.sh
+/root/feelsjournal/scripts/ec2/setup_gunicorn.sh
 
 # Setup Supervisor
-chmod +x /root/metricboard-server/scripts/ec2/setup_supervisor.sh
-/root/metricboard-server/scripts/ec2/setup_supervisor.sh
+chmod +x /root/feelsjournal/scripts/ec2/setup_supervisor.sh
+/root/feelsjournal/scripts/ec2/setup_supervisor.sh
