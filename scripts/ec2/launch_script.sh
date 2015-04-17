1a4027d52b757d5eb32a7b372a5d4c6c02db7d0c
Chris Bates <christopher.andrew.bates@gmail.com>
2015-04-17 08:33:34 -0700

    changes to start script

--- scripts/ec2/launch_script.sh
+++ scripts/ec2/launch_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum -y install git
-git clone https://chrisbates:aassddff90-@github.com/chrisbates/metricboard-server.git /root/feelsjournal
+git clone https://chrisbates:aassddff90-@github.com/chrisbates/feelsjournal.git /root/feelsjournal
 
-chmod +x /root/metricboard-server/scripts/ec2/run_all.sh
-/root/metricboard-server/scripts/ec2/run_all.sh
+chmod +x /root/feelsjournal/scripts/ec2/run_all.sh
+/root/feelsjournal/scripts/ec2/run_all.sh
 
--- scripts/ec2/setup_env.sh
+++ scripts/ec2/setup_env.sh
@@ -3,9 +3,9 @@ curl -o /tmp/ez_setup.py https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_
 /usr/bin/python27 /tmp/ez_setup.py 
 /usr/bin/easy_install-2.7 pip 
 pip install virtualenv
-pip install --upgrade virtualenvwrapper
-echo "source /usr/bin/virtualenvwrapper_lazy.sh" >> /root/.bashrc
-source /root/.bashrc
+#pip install --upgrade virtualenvwrapper
+#echo "source /usr/bin/virtualenvwrapper_lazy.sh" >> /root/.bashrc
+#source /root/.bashrc
 
 mkvirtualenv feelsjournal
 source /root/.virtualenvs/feelsjournal/bin/activate
